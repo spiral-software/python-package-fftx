ad6c233ffbe65da1a59dee55982e30ca82d89eeb
Mike Franusich <mike.franusich@spiralgen.com>
2023-01-13 15:41:07 -0500

    add initial docstring

--- __init__.py
+++ __init__.py
@@ -1,7 +1,27 @@
+"""
+FFTX
+====
+
+This Python package provides NumPy/CuPy-compatible access to the 
+high performance multi-platform code generation capabilities of 
+the FFTX Project (https://github.com/spiral-software/fftx).  Both
+FFTX and this Python package originated under the Exascale Computing
+Project, with a focus on perforance-portable FFTs and FFT-using
+methods for the rapidly evolving high performance computing landscape.
+
+Modules
+-------
+
+fft
+    Compatible subset of NumPy and CuPy fft subpackages
+
+convo
+    Convolutions and convolution-like methods
+"""
 
 from . import fft
 from . import convo
 
-__version__ = '1.0.0a2'
+__version__ = '1.0.0'
 
 
